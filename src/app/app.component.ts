4f68fde880107bcac265820ae589dc943d4944ab
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-09-30 15:17:55 +0700

    structural_directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -8,17 +8,46 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class AppComponent {
   title: string = "tutorial";
-  firstName: string = "Angular";
-  lastName: string = "tutorial";
-  fullName: string = "";
-  getFullName(): string {
-    return this.fullName = this.firstName + ' ' + this.lastName;
-  }
-  @Input() count: number = 0;
-  @Output() countChanged = new EventEmitter<number>();
-  handleIncrement(): void {
-    this.count++;
-    this.countChanged.emit(this.count);
-  }
+  showMe: boolean = true;
+  movies: Movie[] = [
+    {
+      title: 'Zootopia',
+      director: 'Byron Howard, Rich Moore',
+      cast: 'Idris Elba, Ginnifer Goodwin, Jason Bateman',
+      releaseDate: 'March 4, 2016',
+    },
+    {
+      title: 'Batman v Superman: Dawn of Justice',
+      director: 'Zack Snyder',
+      cast: 'Ben Affleck, Henry Cavill, Amy Adams',
+      releaseDate: 'March 25, 2016',
+    },
+    {
+      title: 'Captain American: Civil War',
+      director: 'Anthony Russo, Joe Russo',
+      cast: 'Scarlett Johansson, Elizabeth Olsen, Chris Evans',
+      releaseDate: 'May 6, 2016',
+    },
+    {
+      title: 'X-Men: Apocalypse',
+      director: 'Bryan Singer',
+      cast: 'Jennifer Lawrence, Olivia Munn, Oscar Isaac',
+      releaseDate: 'May 27, 2016',
+    },
+    {
+      title: 'Warcraft',
+      director: 'Duncan Jones',
+      cast: 'Travis Fimmel, Robert Kazinsky, Ben Foster',
+      releaseDate: 'June 10, 2016',
+    },
+  ];
 
+  @Input() num:number = 0;
+}
+
+class Movie {
+  title?: string;
+  director?: string;
+  cast?: string;
+  releaseDate?: string;
 }
