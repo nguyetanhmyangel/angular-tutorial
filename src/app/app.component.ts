5a95d71112ce3203cc7415ea38da528d51ff7070
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-09-30 16:05:05 +0700

    attribute directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,46 +8,19 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   title: string = "tutorial";
-  showMe: boolean = true;
-  movies: Movie[] = [
-    {
-      title: 'Zootopia',
-      director: 'Byron Howard, Rich Moore',
-      cast: 'Idris Elba, Ginnifer Goodwin, Jason Bateman',
-      releaseDate: 'March 4, 2016',
-    },
-    {
-      title: 'Batman v Superman: Dawn of Justice',
-      director: 'Zack Snyder',
-      cast: 'Ben Affleck, Henry Cavill, Amy Adams',
-      releaseDate: 'March 25, 2016',
-    },
-    {
-      title: 'Captain American: Civil War',
-      director: 'Anthony Russo, Joe Russo',
-      cast: 'Scarlett Johansson, Elizabeth Olsen, Chris Evans',
-      releaseDate: 'May 6, 2016',
-    },
-    {
-      title: 'X-Men: Apocalypse',
-      director: 'Bryan Singer',
-      cast: 'Jennifer Lawrence, Olivia Munn, Oscar Isaac',
-      releaseDate: 'May 27, 2016',
-    },
-    {
-      title: 'Warcraft',
-      director: 'Duncan Jones',
-      cast: 'Travis Fimmel, Robert Kazinsky, Ben Foster',
-      releaseDate: 'June 10, 2016',
-    },
-  ];
-
-  @Input() num:number = 0;
+  size: string = '20';
+  styleClass: StyleClass = new StyleClass();
+  cssStringVar: string= 'red size20';
+  cssArray:string[]=['red','size20']; 
 }
 
-class Movie {
-  title?: string;
-  director?: string;
-  cast?: string;
-  releaseDate?: string;
+class StyleClass {
+  'color': string= 'red';
+  'font-size.px': number= 20
+  'font-weight': string= 'bold';
+}
+
+class CssClass {
+  red: boolean= true;
+  size20: boolean= true;
 }
