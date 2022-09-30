d33498cb252c2004ba9062a43fcff04ceb270b1f
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-09-30 10:08:36 +0700

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-// interpolation bindings {{}}
-// property binding []
-// attribute binding [art.]
-
-  title = 'Base Tutorial';
-  info:string = 'Employee Information';
-  id:number = 1;
-  name:string = 'Moonlight';
-  gender:string = 'Male';
-  designation:string = 'Software Developer';
-  location:string = 'Vietnam';
-  salary:number = 100000;
-  imgHeight:number =400;
-  imgWidth:number =400;
-  imgPath:string = 'assets/images/Yuna.jpg';
-  columnSpan: number = 4;
-  isDisabled:boolean = true;
+  title ="";
+  clickCount: number = 0;
+  clickMe(): void {
+    this.clickCount++;
+  }
+  inputValue: string = '';
+  handleInput(event: any): void {
+    this.inputValue = (event.target as HTMLInputElement).value;
+  }
 }
