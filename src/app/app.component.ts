3f8c0d9969264956fb46bc61e55b22dd1f3ac092
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-10-18 09:29:56 +0700

    setvalue-template-driven-forms commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, OnInit, Injectable } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, Injectable, OnChanges } from '@angular/core';
 import { NgForm, FormGroup } from '@angular/forms';
 import { Contact } from './services/contact';
 import { ContactService } from './services/contact.service';
@@ -14,6 +14,7 @@ export class AppComponent {
   title = 'Template driven forms';
   countryList!: Country[];
   contactList!: Contact[];
+  contact!: Contact;
 
   @ViewChild('contactForm') contactForm!: NgForm;
 
@@ -23,13 +24,35 @@ export class AppComponent {
 
   ngOnInit() {
     this.countryList = this.countryService.getAllCountries();
-    this.contactList = this.contactService.getAllContacts();
+    this.contact = this.contactService.getAllContacts();
+    this.contact = this.contactService.getAllContacts();
   }
 
   onSubmit() {
-    console.log(this.contactForm.value);
+    console.log(this.contact);
   }
 
+  setDefaults() {
+    console.log(this.contact);
+    this.contact = this.contactService.getAllContacts();
+    console.log(this.contactService.getAllContacts());
+    // this.contact = {
+    //   firstName: "Tests",
+    //   lastName: "Tendulkar",
+    //   email: "sachin@gmail.com",
+    //   gender: "male",
+    //   isMarried: true,
+    //   country: "2",
+    //   address: { city: "Mumbai", street: "Perry Cross Rd", pinCode: "400050" }
+    // };
+  }
 
+  changeCountry() {
+    this.contact.country = "1";
+  }
+
+  reset(contactForm :NgForm) {
+    contactForm.resetForm();
+  }
 
 }
