6794e9780ad57d8719800d4d04cdbbbc13d67d06
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-10-27 14:36:19 +0700

    1st commit

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,6 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, pluck, switchMap, filter, tap } from 'rxjs';
 import { Article } from '../models/article';
 import { ArticleService } from '../services/article.service';
 
@@ -12,11 +13,28 @@ import { ArticleService } from '../services/article.service';
 export class ArticleDetailComponent implements OnInit {
 
   article$!: Observable<Article>;
-  constructor(private _route: ActivatedRoute, private _api: ArticleService) {}
+  constructor(private _route: ActivatedRoute, private _articleService: ArticleService) {}
 
   ngOnInit(): void {
+    // console.log(this._route.snapshot.paramMap);
+    // console.log(this._route.snapshot.params);
+    // this._route.paramMap.subscribe(console.log);
+    // this._route.params.subscribe(console.log);
+
+    // get the article method one
+    // this.article$ = this._route.params.pipe(
+    //   pluck('slug'),
+    //   switchMap(slug => this._articleService.getArticleBySlug(slug)),
+    //   tap(val => {
+    //     console.log("!val is: " + !val);
+    //     console.log("!!val is: " + !!val);
+    //   }),
+    //   filter((article) => !!article)
+    // );
+
+    // get the article method two
     let slug = this._route.snapshot.paramMap.get('slug')!;
-    this.article$ = this._api.getArticleBySlug(slug);
+    this.article$ = this._articleService.getArticleBySlug(slug);
   }
 
 }
