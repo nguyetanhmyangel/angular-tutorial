13527c2855c7d2ccf6822c8f328699f85419175a
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-11-18 11:23:32 +0700

    http-post commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Person } from './models/person';
+import { ApiService } from './services/api.service';
 
 
 
@@ -9,7 +11,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Routing module';
+  title = 'Http post';
+  people!: Person[];
+  person = new Person();
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.refreshPeople()
+  }
+
+  refreshPeople() {
+    this.apiService.getPeople()
+      .subscribe(data => {
+        console.log(data)
+        this.people = data;
+      })
+
+  }
+
+  addPerson() {
+    this.apiService.addPerson(this.person)
+      .subscribe(data => {
+        console.log(data)
+        this.refreshPeople();
+      })
+  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule, } from '@angular/forms';
-import { ContactComponent } from './contact/contact.component';
-import { ProductComponent } from './product/product.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { HomeComponent } from './home/home.component';
-import { ErrorComponent } from './error/error.component';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
-import { ProductOverviewComponent } from './product-overview/product-overview.component';
-import { ProductSpecComponent } from './product-spec/product-spec.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContactComponent,
-    ProductComponent,
-    ProductDetailComponent,
-    HomeComponent,
-    ErrorComponent,
-    ProductOverviewComponent,
-    ProductSpecComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-
-    RouterModule.forRoot(appRoutes)                           /*path location strategy */
-    /*RouterModule.forRoot(appRoutes, { useHash: true }) */   /*Hashlocationstrategy */
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
