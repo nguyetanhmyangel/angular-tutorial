e99e10af4465d720e6dde34518dc8a395bb9b930
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2022-11-22 10:01:20 +0700

    passing-optional-query-parameters-to-route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 
 
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Routing module';
+  title = 'Routing tutorial';
+  pageNum = 0;
 }
 
 
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
 
@@ -8,14 +9,27 @@ import { ProductService } from '../services/product.service';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  pageNo = 0;
+  snapshotPageNo = 0;
 
-  products!:Product[];
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
-   constructor(private productService:ProductService){
-   }
+  ngOnInit() {
+    this.snapshotPageNo = +this.activatedRoute.snapshot.queryParamMap.get('pageNum')! || 0;
 
-   ngOnInit() {
-     this.products=this.productService.getProducts();
-   }
+    this.activatedRoute.queryParamMap.subscribe((params) => {
+      this.pageNo = +params.get('pageNum')! || 0;
+      console.log('Query params ', this.pageNo);
+    });
+  }
+
+  nextPage() {
+    this.router.navigate(['product'], {
+      queryParams: { pageNum: this.pageNo + 1 },
+    });
+
+    //You can also use the navigateByUrl
+    //this.router.navigateByUrl('product?pageNum='+(+this.pageNo+1),);
+  }
 
 }
